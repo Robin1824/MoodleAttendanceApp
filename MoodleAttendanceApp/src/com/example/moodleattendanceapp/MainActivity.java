a34347cd7b25cc24e6d314a3455033fa11d1042f
Jaydev Mehta <Jaydev Mehta@MAHADEV-PC>
2015-06-27 10:23:57 +0530

    Login Screen set Validation of Internet and check blank edittext.

--- MoodleAttendanceApp/src/com/example/moodleattendanceapp/MainActivity.java
+++ MoodleAttendanceApp/src/com/example/moodleattendanceapp/MainActivity.java
@@ -2,36 +2,129 @@ package com.example.moodleattendanceapp;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	int a,b,c;
 	ActionBar mActionBar;
-	EditText txtUserName,txtPassword;
-	TextView tv;
+	EditText etUserName, etPassword;
+	Button btnUserLogin;
+	
+	// flag for Internet connection status
+    Boolean isInternetPresent = false;
+     
+    // Connection detector class
+    ConnectionDetector cd;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		SetActionBar();
+		etUserName = (EditText) findViewById(R.id.etUsername);
+		etPassword = (EditText) findViewById(R.id.etPassword);
+		btnUserLogin = (Button) findViewById(R.id.btnLogin);
+		
+		// creating connection detector class instance
+        cd = new ConnectionDetector(getApplicationContext());
+
+		btnUserLogin.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				UserLogin(v);
+			}
+		});
 	}
-	
+
 	public void SetActionBar() {
 		mActionBar = getActionBar();
-		//mActionBar.setDisplayShowHomeEnabled(false);
+		// mActionBar.setDisplayShowHomeEnabled(false);
 		mActionBar.setDisplayShowTitleEnabled(true);
 		mActionBar.setBackgroundDrawable(new ColorDrawable(Color
 				.parseColor("#FFB917")));
 		mActionBar.setTitle("Login");
 	}
 
+	public void UserLogin(View v) {
+
+		if (!isEmpty(etUserName) && !isEmpty(etPassword)) {
+			//make HTTP request
+			
+			// get Internet status
+            isInternetPresent = cd.isConnectingToInternet();
+
+            // check for Internet status
+            if (isInternetPresent) {
+                // Internet Connection is Present
+                // make HTTP requests
+                openAlert("Internet Connection","You have internet connection");
+            } else {
+                // Internet connection is not present
+                // Ask user to connect to Internet
+                openAlert("No Internet Connection","You don't have internet connection.");
+            }
+			
+			/*
+			 * uname = et_Uname.getText().toString().toUpperCase(); pwd =
+			 * et_pwd.getText().toString();
+			 */
+
+			/*
+			 * mEditor.putString("URL", url); mEditor.putString("Username",
+			 * uname); mEditor.putString("Password", pwd);
+			 * mEditor.putString("OrgName", orgname); mEditor.putString("OrgID",
+			 * orgid); mEditor.commit();
+			 */
+			Log.i("orgid save", "ok");
+
+		} else {
+			if (isEmpty(etUserName)) {
+				etUserName.setHint("Enter Username");
+				openAlert("Message","Please Enter Username");
+			}
+			else if (isEmpty(etPassword)) {
+				etPassword.setHint("Enter Password");
+				openAlert("Message","Please Enter Password");
+			}
+		}
+	}
+
+	private boolean isEmpty(EditText etText) {
+		return etText.getText().toString().trim().length() == 0;
+	}
+
+	private void openAlert(String mTitle,String msg) {
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+				MainActivity.this);
+
+		alertDialogBuilder.setTitle(mTitle);
+		alertDialogBuilder.setMessage(msg);
+		// set positive button: Yes message
+		alertDialogBuilder.setPositiveButton("OK",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+
+					}
+				});
+
+		AlertDialog alertDialog = alertDialogBuilder.create();
+		// show alert
+		alertDialog.show();
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
